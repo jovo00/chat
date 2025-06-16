03c940439f2fa1321f2c91ef64234ae50db7e87f
jovo00 <tulpenweg@mail.de>
2025-06-16 21:20:49 +0200

    google auth

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -35,7 +35,7 @@ export default function Login() {
           </div>
 
           <SignInWithGoogle />
-          <SignInWithGithub />
+          {/* <SignInWithGithub /> */}
         </div>
       </main>
     </div>
